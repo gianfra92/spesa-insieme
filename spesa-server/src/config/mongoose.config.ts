1cd91550a4c73351be9825ee34b62d67bd66ef6a
gianfra92 <gianfra92@gmail.com>
2025-05-23 18:31:36 +0200

    updated files to prettierc

--- spesa-server/src/config/mongoose.config.ts
+++ spesa-server/src/config/mongoose.config.ts
@@ -2,10 +2,3 @@ export default {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 };
-
-// src/common/dto/base-response.dto.ts
-export class BaseResponseDto<T> {
-  success: boolean;
-  data?: T;
-  message?: string;
-}
